87d3601e38e58198c27f0054517e6d9e82ea8a67
Artem Naypak <naypakapoka@mail.ru>
2022-11-10 19:25:38 +0300

    The third HW is has been done

---
+++ 3_Lesson_HW/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Без использования строк
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = Prompt("enter a five-digit number: ");
+if (number >= 100000 || number < 10000)
+{
+    System.Console.WriteLine("Are you sure you've enterd a five-digit number?");
+}
+else
+{
+    int temp = number;
+    int rev = 0;
+    while (number > 0)
+    {
+        int dig = number % 10;
+        rev = rev * 10 + dig;
+        number = number / 10;
+    }
+    if(temp == rev)
+    {
+        System.Console.WriteLine("that's a palindrome");
+    }
+    else
+    {
+        System.Console.WriteLine("that's not a palindrome");
+    }
+}
---
+++ 3_Lesson_HW/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int Prompt(string message)
+{
+System.Console.Write(message);
+return int.Parse(Console.ReadLine());
+}
+int x1 = Prompt("enter the coordinate X of a point A: ");
+int y1 = Prompt("enter the coordinate Y of a point A: ");
+int w1 = Prompt("enter the coordinate W of a point A: ");
+
+int x2 = Prompt("enter the coordinate X of a point B: ");
+int y2 = Prompt("enter the coordinate Y of a point B: ");
+int w2 = Prompt("enter the coordinate W of a point B: ");
+
+int x = x1 - x2;
+int y = y1 - y2;
+int w = w1 - w2;
+double distance = Math.Sqrt(x * x + y * y + w * w);
+System.Console.WriteLine($"{distance:f2}");
