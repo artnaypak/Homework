7caec375e8363f4d9c77bbdc67494652ec8b03ba
Artem Naypak <naypakapoka@mail.ru>
2022-12-13 20:43:28 +0300

    Hometask №9 is done

---
+++ 9_Lesson_HW/Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void PrintEvenNumbersMN(int m, int n)
+{
+    if (n < m)
+    {
+        return;
+    }
+    if (m % 2 == 0)
+    {
+        Console.WriteLine(m);
+    }
+    PrintEvenNumbersMN(m + 1, n);
+}
+
+int m = 3;
+int n = 16;
+PrintEvenNumbersMN(m, n);
---
+++ 9_Lesson_HW/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+void ShowSumMinMax(int min, int max, int sum)
+{
+    sum += min;
+    if (max <= min)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    ShowSumMinMax(min + 1, max, sum);
+}
+
+int min = 1;
+int max = 100;
+int sum = 0;
+ShowSumMinMax(min, max, sum);
